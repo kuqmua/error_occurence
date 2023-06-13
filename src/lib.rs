0e1f647a74f442f22f3deb325f397d2d9d13eac2
kuqmua <serezhashmakov@outlook.com>
2023-06-13 08:21:45 +0300

    lil reusage

--- src/lib.rs
+++ src/lib.rs
@@ -958,6 +958,19 @@ pub fn error_occurence(
                             let vec_display_into_vec_string_token_stream = vec_display_into_vec_string_lower_case
                             .parse::<proc_macro2::TokenStream>()
                             .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {vec_display_into_vec_string_lower_case} {parse_proc_macro2_token_stream_failed_message}"));
+                            //
+                            let hashmap_display_display_into_hashmap_string_string_camel_case = "HashMapDisplayDisplayIntoHashMapStringString";
+                            let hashmap_display_display_into_hashmap_string_string_lower_case = hashmap_display_display_into_hashmap_string_string_camel_case
+                            .to_case(convert_case::Case::Snake)
+                            .to_lowercase();
+                            let hashmap_display_display_into_hashmap_string_string_token_stream = hashmap_display_display_into_hashmap_string_string_lower_case
+                            .parse::<proc_macro2::TokenStream>()
+                            .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {hashmap_display_display_into_hashmap_string_string_lower_case} {parse_proc_macro2_token_stream_failed_message}"));
+                            let crate_common_error_logs_logic_hashmap_display_display_into_hashmap_string_string_hash_map_display_display_into_hashmap_string_string_stringified = format!("{crate_common_stringified}::{error_logs_logic_stringified}::{hashmap_display_display_into_hashmap_string_string_lower_case}::{hashmap_display_display_into_hashmap_string_string_camel_case}");
+                            let crate_common_error_logs_logic_hashmap_display_display_into_hashmap_string_string_hash_map_display_display_into_hashmap_string_string_token_stream = 
+                            crate_common_error_logs_logic_hashmap_display_display_into_hashmap_string_string_hash_map_display_display_into_hashmap_string_string_stringified
+                            .parse::<proc_macro2::TokenStream>()
+                            .unwrap_or_else(|_| panic!("{proc_macro_name_ident_stringified} {crate_common_error_logs_logic_hashmap_display_display_into_hashmap_string_string_hash_map_display_display_into_hashmap_string_string_stringified} {parse_proc_macro2_token_stream_failed_message}"));
                             let (
                                 logic_for_source_to_string_with_config_for_attribute, 
                                 logic_for_source_to_string_without_config_for_attribute,
@@ -1877,8 +1890,8 @@ pub fn error_occurence(
                                                 },
                                                 quote::quote! {
                                                     {
-                                                        use crate::common::error_logs_logic::hashmap_display_display_into_hashmap_string_string::HashMapDisplayDisplayIntoHashMapStringString;
-                                                        #field_ident.hashmap_display_display_into_hashmap_string_string()
+                                                        use #crate_common_error_logs_logic_hashmap_display_display_into_hashmap_string_string_hash_map_display_display_into_hashmap_string_string_token_stream;
+                                                        #field_ident.#hashmap_display_display_into_hashmap_string_string_token_stream()
                                                     }
                                                 }
                                             )
@@ -2182,8 +2195,8 @@ pub fn error_occurence(
                                                     },
                                                     quote::quote! {
                                                         {
-                                                            use crate::common::error_logs_logic::hashmap_display_display_into_hashmap_string_string::HashMapDisplayDisplayIntoHashMapStringString;
-                                                            #field_ident.hashmap_display_display_into_hashmap_string_string()
+                                                            use ;#crate_common_error_logs_logic_hashmap_display_display_into_hashmap_string_string_hash_map_display_display_into_hashmap_string_string_token_stream
+                                                            #field_ident.#hashmap_display_display_into_hashmap_string_string_token_stream()
                                                         }
                                                     }
                                                 )
